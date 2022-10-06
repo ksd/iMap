a9ecd7e75e3c69bdbc047e2e7cdf99faf398be5e
Kaj Schermer Didriksen <kdidriksen@me.com>
2022-10-06 10:32:12 +0200

    Initial Commit

---
+++ iMap/iMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMapApp.swift
+//  iMap
+//
+//  Created by ksd on 06/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct iMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
